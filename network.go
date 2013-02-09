0b170b73f352950bd21cc9d5904356b49cb1784f
Sasha Crofter <sasha@crofter.org>
2013-02-09 14:50:26 -0500

    Add JSON tags

--- network.go
+++ network.go
@@ -4,25 +4,31 @@ package network
 // meant to represent an entire (not subdivided) network of individual
 // nodes, each with attributes.
 type Network struct {
-	Nodes map[string]*Node
+	Nodes map[string]*Node `json:"nodes"`
 }
 
 // A Node is a single node in a network with a set of connections and
 // attributes.
 type Node struct {
-	Connected []string `json:"connected"`
+	Connected []*Connection `json:"connected"`
 	// All of the connected nodes
 	Attributes map[string]interface{} `json:"attributes,omitempty"`
 	// Any attributes
 
-	Disabled bool // Whether the node is currently inactive
-	ShouldBe bool // Whether the node should be inactive
+	Disabled bool `json:"disabled,omitempty"`
+	// Whether the node is currently inactive
+	ShouldBe bool `json:"shouldbe,omitempty"`
+	// Whether the node should be inactive
 }
 
 type Connection struct {
-	Target  string // Name of connected node
-	Quality uint64 // A unitless "opinion" of the quality of the link
+	Target string `json:"target"`
+	// Name of connected node
+	Quality uint64 `json:"quality,omitempty"`
+	// A unitless "opinion" of the quality of the link
 
-	Disabled bool // Whether the connection is currently inactive
-	ShouldBe bool // Whether the connection should be inactive
+	Disabled bool `json:"disabled,omitempty"`
+	// Whether the connection is currently inactive
+	ShouldBe bool `json:"shouldbe,omitempty"`
+	// Whether the connection should be inactive
 }
